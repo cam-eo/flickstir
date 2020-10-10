a2fdcde270f20fef1612bcc3838d52d0cdec68f2
cam <cameron.n.wright@gmail.com>
2020-10-10 10:52:26 +0200

    firebase config file

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAAmz8pWqqFnQmKwPimML9ynE51X3Zet8A",
+    authDomain: "flick-stir.firebaseapp.com",
+    databaseURL: "https://flick-stir.firebaseio.com",
+    projectId: "flick-stir",
+    storageBucket: "flick-stir.appspot.com",
+    messagingSenderId: "1061463430728",
+    appId: "1:1061463430728:web:70743a79176c3b9f9e2a7d",
+    measurementId: "G-L8FDH5T13Q"
+  };
